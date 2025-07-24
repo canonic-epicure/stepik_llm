eef594a839de87f3968a5ef44abfc4f882ce8e00
nickolay <nickolay8@gmail.com>
2025-07-24 16:29:05 +0300

    some progress

--- gpt.py
+++ gpt.py
@@ -1,4 +1,6 @@
 import \
+    time
+import \
     torch
 import \
     tqdm
@@ -91,23 +93,41 @@ class GPT(nn.Module):
 
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
 
-        for e in tqdm.tqdm(range(self.epoch, self.epoch + num_epoch)):
+        for e in range(self.epoch, self.epoch + num_epoch):
+            print(f'Starting epoch {e}')
+            epoch_start = time.perf_counter()
+
             self.train()
 
             loss = []
 
+            step = 0
+            total_time = 0
+
             for inputs, targets in train_loader:
+                start = time.perf_counter()
+
                 res = self.forward(inputs)
 
                 res_mod = res.reshape(res.shape[0] * res.shape[1], -1)
                 targets_mod = targets.reshape(targets.shape[0] * targets.shape[1])
 
                 train_loss = torch.nn.functional.cross_entropy(res_mod, targets_mod)
-                loss.append(train_loss)
+                loss.append(train_loss.item())
 
                 optimizer.zero_grad()
                 train_loss.backward()
                 optimizer.step()
+
+                end = time.perf_counter()
+
+                step += 1
+                step_time = end - start
+                total_time += step_time
+
+                print(f'\rtrain step={ step } done, from {len(train_loader)}, time={ step_time }s, loss={ train_loss.item() }, time remain={ (len(train_loader) - step) * (total_time / step) }s', end='', flush=True)
+
+            print()
 
             train_loss = torch.mean(torch.tensor(loss)).item()
 
@@ -115,8 +135,13 @@ class GPT(nn.Module):
 
             loss = []
 
+            step = 0
+            total_time = 0
+
             with torch.no_grad():
                 for inputs, targets in valid_loader:
+                    start = time.perf_counter()
+
                     res = self.forward(inputs)
 
                     res_val = res.reshape(res.shape[0] * res.shape[1], -1)
@@ -124,11 +149,30 @@ class GPT(nn.Module):
 
                     valid_loss = torch.nn.functional.cross_entropy(res_val, targets_val)
 
-                    loss.append(valid_loss)
+                    loss.append(valid_loss.item())
+
+                    end = time.perf_counter()
+
+                    step += 1
+                    step_time = end - start
+                    total_time += step_time
+
+                    print(f'\rvalid step={ step } done, from {len(valid_loader)}, time={ step_time }s, loss={ valid_loss.item() }, time remain={ (len(valid_loader) - step) * (total_time / step) }s', end='', flush=True)
 
             print(f'train_loss={ train_loss } valid_loss={ torch.mean(torch.tensor(loss)).item() }')
 
-            self.save(f'./models/model_{ str(e).rjust(3, "0") }.pt')
+            epoch_end = time.perf_counter()
+            print(f'Ending epoch {e}, time={ epoch_end - epoch_start }s')
+
+            start = time.perf_counter()
+
+            model_file = f'./models/model_{ str(e).rjust(3, "0") }.pt'
+
+            self.save(model_file)
+
+            end = time.perf_counter()
+
+            print(f'\rEnding epoch {e}, time={ epoch_end - epoch_start }s, model saved to { model_file } within { end - start }s', end='', flush=True)
 
     def save(self, path):
         torch.save({
--- train_start.py
+++ train_start.py
@@ -7,6 +7,7 @@ from torch.utils.data import \
 import bpe as bpe
 import config
 import time
+import re
 
 import \
     gpt
@@ -29,13 +30,32 @@ train_token_ids = input_tokens[:n]
 valid_token_ids = input_tokens[n:]
 
 train_dataset = GetData(data=train_token_ids, seq_len=config.seq_len, device=device)
-train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=config.batch_size) # , shuffle=True ??
 
 valid_dataset = GetData(data=valid_token_ids, seq_len=config.seq_len, device=device)
-valid_loader = DataLoader(valid_dataset, batch_size=config.batch_size, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=config.batch_size) # , shuffle=True ??
 
-model = gpt.GPT(config.vocab_size, config.max_seq_len, config.emb_size, config.num_heads, config.head_size, config.num_layers, config.dropout, device=device)
+if False:
+    model = gpt.GPT(config.vocab_size, config.max_seq_len, config.emb_size, config.num_heads, config.head_size, config.num_layers, config.dropout, device=device)
 
-model.tokenizer = tokenizer
+    # model.tokenizer = tokenizer
+else:
+    files = glob.glob(f'{ current_dir }/models/model_*.pt')
+
+    pattern = re.compile(r"model_(\d+)\.pt$")
+
+    epochs = [ pattern.search(file)[1] for file in files if pattern.search(file) != None ]
+
+    if len(epochs) == 0:
+        print('no models found')
+        exit(0)
+
+    epochs.sort(reverse=True)
+
+    model = gpt.GPT.load(f'{ current_dir }/models/model_{ epochs[ 0] }.pt', device=device)
+    # model.device = device
+    model.epoch = int(epochs[ 0 ]) + 1
+
+model = model.compile()
 
 model.fit(train_loader, valid_loader, config.num_epoch, config.learning_rate)
